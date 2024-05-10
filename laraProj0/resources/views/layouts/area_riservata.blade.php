c97107295e57be8c82a28e026b56bd32750fd479
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-10 14:42:35 +0200

    altre prove

---
+++ laraProj0/resources/views/layouts/area_riservata.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>NeuroClinic | @yield('title') </title>
+</head>
+<body class="bg-cyan-50">
+    <header>
+        <div class="bg-cyan-600 h-[100px] flex items-center justify-between">
+            <div>
+                <img src="images/logo_bianco.svg" class="h-16" alt="Logo">  <!-- qui va aggiunto l'href alla homepage -->
+            </div>
+        </div>
+    </header>
+</body>
+</html>
