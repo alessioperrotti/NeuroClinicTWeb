4b88ebe5ddc16203e443dab11a281d8b4892f249
andreaperugini <andreaperugini74@gmail.com>
2024-05-14 17:57:21 +0200

    Update home_admin.blade.php

--- laraProj0/resources/views/home_admin.blade.php
+++ laraProj0/resources/views/home_admin.blade.php
@@ -2,29 +2,59 @@
 
 @section('content')
 
-<h1>Benvenuto, Admin</h1>
-<div class="flex justify-around">
-    <div class="m-4 ">
-        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md max-96">
+<h1 class="p-10 text-5xl font-bold">Benvenuto, Admin</h1>
+<div class="flex justify-center">
+    <div class="m-8 ">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
             <a href=>
-                <img src="images/dottore.svg" class="size-96" alt="Clinici">
+                <img src="images/dottore.svg" class="size-60" alt="Clinici">
             </a>
         </div>
-        <h2>Gestione clinici</h2>
+        <h2 class="p-2 text-3xl" >Gestione clinici</h2>
     </div>
-    <div class="bg-[#0097B2]/15 border-2 border-black rounded-md max-h-96">
-        <a>
-            <img src="images/dottore.svg" class="size-96" alt="Clinici">
-        </a>
+    <div class="m-8 ">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
+            <a href=>
+                <img src="images/medicine.svg" class="size-60" alt="Clinici">
+            </a>
+        </div>
+        <h2 class="p-2 text-3xl">Gestione clinici</h2>
     </div>
-    <div class="bg-[#0097B2]/15 border-2 border-black rounded-md">
-        <a>
-            <img src="images/dottore.svg" class="size-96" alt="Clinici">
-        </a>
+    <div class="m-8 ">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
+            <a href=>
+                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+            </a>
+        </div>
+        <h2 class="p-2 text-3xl">Gestione clinici</h2>
+    </div>
+</div>
 
+
+<div class="flex justify-center">
+    <div class="m-8 text-3xl">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
+            <a href=>
+                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+            </a>
+        </div>
+        <h2 class="p-2 text-3xl">Gestione clinici</h2>
     </div>
-    <div>
-        <img>
+    <div class="m-8 text-3xl">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
+            <a href=>
+                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+            </a>
+        </div>
+        <h2 class="p-2 text-3xl">Gestione clinici</h2>
+    </div>
+    <div class="m-8">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
+            <a href=>
+                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+            </a>
+        </div>
+        <h2 class="p-2 text-3xl">Gestione clinici</h2>
     </div>
 </div>
 
