c81117870bab27a79e6b02f1e47b857af524a52e
andreaperugini <andreaperugini74@gmail.com>
2024-05-15 13:23:26 +0200

    home_admin finita

--- laraProj0/resources/views/home_admin.blade.php
+++ laraProj0/resources/views/home_admin.blade.php
@@ -5,9 +5,9 @@
 <h1 class="p-10 text-5xl font-bold">Benvenuto, Admin</h1>
 <div class="flex justify-center">
     <div class="m-8 ">
-        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
-            <a href=>
-                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+        <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square size-60 ">
+            <a href= "">
+                <img src="images/question.svg" class="size-60" alt="Clinici">
             </a>
         </div>
         <h2 class="p-2 text-3xl" >Gestione clinici</h2>
@@ -15,7 +15,7 @@
     <div class="m-8 ">
         <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
             <a href=>
-                <img src="images/medicine.svg" class="size-60" alt="Clinici">
+                <img src="images/medicina.svg" class="size-60" alt="Clinici">
             </a>
         </div>
         <h2 class="p-2 text-3xl">Farmaci e attività<br>riabilitative</h2>
@@ -23,7 +23,7 @@
     <div class="m-8 ">
         <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
             <a href=>
-                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+                <img src="images/grafico.svg" class="size-60" alt="Clinici">
             </a>
         </div>
         <h2 class="p-2 text-3xl">Analisi dati</h2>
@@ -35,7 +35,7 @@
     <div class="m-8 text-3xl">
         <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
             <a href=>
-                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+                <img src="images/icona_stamp.svg" class="size-60" alt="Clinici">
             </a>
         </div>
         <h2 class="p-2 text-3xl">Gestione disturbi<br>motori</h2>
@@ -43,7 +43,7 @@
     <div class="m-8 text-3xl">
         <div class="bg-[#0097B2]/15 border-2 border-black rounded-md aspect-square h-auto w-auto">
             <a href=>
-                <img src="images/dottore.svg" class="size-60" alt="Clinici">
+                <img src="images/elimina_paziente.svg" class="size-60" alt="Clinici">
             </a>
         </div>
         <h2 class="p-2 text-3xl">Elimina pazienti</h2>
