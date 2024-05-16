b39aa14b276c2775eeb5827289eaefcc5b800c0a
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-16 12:12:33 +0200

    fine login e inizio faq

--- laraProj0/resources/views/faq.blade.php
+++ laraProj0/resources/views/faq.blade.php
@@ -1,1 +1,23 @@
 @extends('layouts.basic')
+
+@section('title', 'FAQ')
+
+@section('content')
+<div class="flex flex-row flex-nowrap">
+    <div name="textring" class="basis-1/3 items-center mt-10">
+        <div class="font-bold text-5xl absolute z-10 pl-16 pt-36 text-gray-800">
+            <p class="mb-4">Frequently</p>
+            <p class="mb-4">Asked</p>
+            <p>Questions</p>
+        </div>
+        <img src="{{ url('images/anello.png')}}" class="z-0 h-[500px] pt-5" >
+    </div>
+    <div name="q&a" class="basis-2/3">
+        <!-- prototipo card q&a -->
+        <!-- foreach {echo } -->
+        <div name="card q&a" class="bg-white shadow-inner rounded-2xl h-24 my-3 pr-8">
+
+        </div>
+    </div>
+</div>
+@endsection
--- laraProj0/resources/views/login.blade.php
+++ laraProj0/resources/views/login.blade.php
@@ -3,33 +3,23 @@
 @section('title', 'Login')
 
 @section('content')
-
 <div class="flex place-content-center justify-center">
-    <div class="bg-white rounded-xl h-[500px] w-[400px] absolute z-10 p-8 mt-[100px]">
-        <h1 class="font-bold font-sans text-3xl">Accedi</h1>
-        <form>
-            <label for="user" class="font-sans my-2">Username</label>
-            <br>
-            <input id="user" type="text" class="border rounded-sm">
-            <br>
-            <label for="user" class="">Password</label>
-            <br>
-            <input id="user" type="text" class="border rounded-sm">
+    <div class="bg-white rounded-xl h-[480px] w-[400px] absolute z-10 p-8 mt-[100px] items-center">
+        <h1 class="font-bold font-sans text-3xl text-center mb-12">Accedi</h1>
+        <form class="flex flex-col items-center">  
+            <div class="mb-8">
+                <label for="user" class="text-xl font-semibold">Username</label>
+                <br>
+                <input id="user" type="text" class="border rounded-md h-[60px] w-[336px] p-4 text-xl">
+            </div>
+            <div class="mb-8"> 
+                <label for="user" class="text-xl font-semibold">Password</label>
+                <br>
+                <input id="user" type="password" class="border rounded-md h-[60px] w-[336px] p-4 text-xl">
+            </div>
+            <input type="submit" class="bg-cyan-600 rounded-xl mt-3 w-[200px] h-[60px] text-white font-semibold" value="Login">   
         </form>
     </div>
     <img src="{{ url('images/star3d.png')}}" class="h-[700px] z-0" alt="Sfondo Login">
 </div>
-
-<!--
-<div class="flex justify-center items-center h-screen">
-    
-    <div class="bg-blue-500 rounded-lg p-8 shadow-lg absolute z-10">
-        
-        <p>Contenuto del rettangolo</p>
-    </div>
-    
-    <img src="{{ url('images/star3d.png')}}" alt="Immagine" class="rounded-full border-4 border-white shadow-lg z-0">
-</div>
--->
-
 @endsection
