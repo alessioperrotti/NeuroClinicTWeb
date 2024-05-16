9330a1269591c733d833c3e234ce52452977a13c
L1me26 <147258488+L1me26@users.noreply.github.com>
2024-05-16 17:04:29 +0200

    Home paziente e relative immagini

--- laraProj0/resources/views/homePaziente.blade.php
+++ laraProj0/resources/views/homePaziente.blade.php
@@ -12,7 +12,7 @@
 </head>
 
 <body class="bg-cyan-50">
-<header class="fixed top-0 left-0 right-0 z-20">  
+    <header class="fixed top-0 left-0 right-0 z-20">  
         <div class="bg-cyan-600 h-[100px] flex items-center justify-between p-8">
             <div> 
                 <img src="images/logo_bianco.svg" class="h-16" alt="Logo">
@@ -25,11 +25,58 @@
             </div>
         </div>
     </header>
-    <h1 class="text-black font-extrabold text-2xl">Benvenuto, Mario Rossi</h1>
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl">
-        
-        <div class="p-4 bg-cyan-600 ">
-
+   
+    <h1 class="text-black font-bold text-5xl mx-8 mt-32">Benvenuto, Mario Rossi</h1>
+    <br>
+    <div class="p-8 max-w-5xl mx-auto bg-white rounded-xl shadow-lg">
+        <h1 class="text-black text-3xl">Dati anagrafici</h1>
+        <br>
+        <p class="text-black font-bold text-base">Nome</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Cognome</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Data di nascita</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Genere</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Indirizzo</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Telefono</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Indirizzo e-mail</p>
+        <br>
+        <br>
+        <div class=" flex justify-center items-center">
+            <button class="p-3 bg-cyan-600 rounded-lg text-white hover:bg-cyan-500 ">
+                Aggiorna i tuoi dati
+            </button>
+        </div>
+    </div>
+    <hr class="h-1 my-10 bg-cyan-600 m-28">
+    <div class="p-8 max-w-5xl mx-auto bg-white rounded-xl shadow-lg">
+        <h1 class="text-black text-3xl">Account</h1>
+        <br>
+        <p class="text-black font-bold text-base">Username</p>
+        <hr class="h-0.5 my-2 bg-cyan-600">
+        <p class="text-black font-bold text-base">Password</p>
+        <br>
+        <div class=" flex justify-center items-center">
+            <button class="p-3 bg-cyan-600 rounded-lg text-white hover:bg-cyan-500 ">Cambia password</button>
+        </div>
+    </div>
+    <hr class="h-1 my-10 bg-cyan-600 m-28">
+    <div class="mx-64">
+        <div class="max-w-96 float-left   mb-10 ">
+            <button class="bg-cyan-100 border-2 border-cyan-600 shadow-xl rounded-md">
+                <img src="images\cartella_clinica.png" alt="Cartella clinica">
+            </button>
+            <p class="text-center text-4xl text-black">Cartella clinica</p>
+        </div>
+        <div class="max-w-96 float-right mb-10 ">
+            <button class="bg-cyan-100 border-2 border-cyan-600 shadow-xl rounded-md">
+                <img src="laraProj0\public\images\nuovo_evento.png" alt="Nuovo evento">
+            </button>
+            <p class="text-center text-4xl text-black">Nuovo evento</p>
         </div>
     </div>
 </body>
