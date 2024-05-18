d5eebe9495e66e887a0663f3302f3d15e9db15e4
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-18 13:29:54 +0200

    Vista Lista Pazienti (per cartelle clin)

--- laraProj0/resources/views/listaPazienti.blade.php
+++ laraProj0/resources/views/listaPazienti.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.basic')
 
-@section('title', 'Gestione clinici')
+@section('title', 'Lista Pazienti')
 <!-- sostituire id con name nell'input di ricerca anche su gestioneClinici-->
 
 @section('content')
 <div class="flex justify-center">
     <h1 class="text-5xl font-bold ml-5 mt-5 mb-8 gap-y-5">Cartelle cliniche</h1>
 </div>
-<div id="app" class=" text-lg container mx-auto p-4 w-full max-w-4xl">
+<div class=" text-lg container mx-auto p-4 w-full max-w-4xl">
     <input type="text" name="cognomePaziente" placeholder="Cerca per cognome" 
     class=" bg-cyan-50 my-6 appearance-none w-full py-2 px-3
     border-0 border-b-2 border-gray-300 focus:border-black
      text-gray-700 leading-tight  focus:outline-none" >
-               
-    <form id="listaPazienti" class="mb-4">
 
-        <div class="flex justify-between items-center bg-white p-2 rounded-lg mb-2">
-            <span class="font-bold">Mario Rossi</span>
+    @isset($pazienti)
+    @foreach ($pazienti as $paziente)
+    <!-- ho messo padding a 4 invece che a 2 (eventualmente cambiare anche su admin) -->
+        
+        <div class="flex justify-between items-center bg-white p-4 rounded-lg mb-2">
+            <span class="font-bold">{{ $paziente->nome }}</span>
             <div class="flex mr-2 gap-x-4">
-                <alt href="">
-                    <img src="{{ url('images/cartella_clinica.png') }}" alt="Modifica" class="w-6 h-6 inline-block">
-                </alt>
+                <a href="">
+                    <img src="{{ url('images/cartella_clinica.png') }}" alt="Modifica" class="h-10 inline-block">
+                </a>
             </div>
         </div>
-        <!-- da aggiungere clinici -->
-    </form>
+
+    @endforeach
+    @endisset()
+    <!-- da aggiungere clinici -->
+
 
     <div class="flex justify-center  mt-10">
         <!-- Bottone per aggiungere un nuovo clinico -->
