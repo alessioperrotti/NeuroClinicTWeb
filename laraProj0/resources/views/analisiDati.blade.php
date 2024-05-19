084f85f53d57435b17348c9a43bec5a0a5416d6b
andreaperugini <andreaperugini74@gmail.com>
2024-05-19 22:42:05 +0200

    b

--- laraProj0/resources/views/analisiDati.blade.php
+++ laraProj0/resources/views/analisiDati.blade.php
@@ -11,7 +11,7 @@
 
     <div class="flex">
 
-        <div class='bg-white text-lg w-1/2 pr-10 '>
+        <div class='bg-white text-lg w-1/2 px-5 '>
             <div class='flex justify-between px-5 gap-x-4 py-3'>
                 <h2 class='w-1/3 font-semibold'>Media pazienti per clinico</h2>
                 <h2>10</h2>
@@ -24,12 +24,15 @@
             <hr>
             <div class='flex justify-between px-5 gap-x-4 py-3'>
                 <h2 class='w-1/3 font-semibold'>Disturbo motorio</h2>
-                <h2>10</h2>
+                <select class="w-1/4">
+                    <option>Prova1</option>
+                    <option>Prova2</option>
+                </select>
             </div>
 
             <div class='flex justify-between px-5 gap-x-4 py-3'>
                 <h2 class='w-1/3 font-semibold'>Numero di eventi totali registrati</h2>
-                <h2>10</h2>
+                <h2 >10</h2>
             </div>
             <hr>
         </div>
