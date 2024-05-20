ae623d5ebaa177a3299f12aaf7f5ff98ed950744
L1me26 <147258488+L1me26@users.noreply.github.com>
2024-05-20 17:22:25 +0200

    aggiorna dati paz

--- laraProj0/resources/views/aggiornaDatiPaziente.blade.php
+++ laraProj0/resources/views/aggiornaDatiPaziente.blade.php
@@ -9,43 +9,44 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-12">
                 <div>
                     <label class="block text-gray-700 font-semibold">Nome</label>
-                    <input type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('nomePaz')>
+                    <input name="nome" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('nomePaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">Cognome</label>
-                    <input type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('cognPaz')>
+                    <input name="cognome" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('cognPaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">Data di nascita</label>
-                    <input type="date" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('dataPaz')>
+                    <input name="dataNascita" type="date" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('dataPaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">Genere</label>
-                    <select size="1" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('genPaz')>
+                    <select name="genere" size="1" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('genPaz')>
                         <option value="M">Maschio</option>
                         <option value="F">Femmina</option>
+                        <option value="A">Altro</option>
                     </select>
                 </div>
                 <div>
                     <label class="block text-gray-700">Indirizzo</label>
-                    <input type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('indirPaz')>
+                    <input name="indirizzo" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('indirPaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">Telefono</label>
-                    <input type="tel" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('telPaz')>
+                    <input name="telefono" type="tel" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('telPaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">E-Mail</label>
-                    <input type="email" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('emailPaz')>
+                    <input name="email" type="email" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('emailPaz')>
                 </div>
                 <div>
                     <label class="block text-gray-700">Username</label>
-                    <input type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('userPaz')>
+                    <input name="username" type="text" class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder=@yield('userPaz')>
                 </div>
             </div>
             <div class="flex justify-center mt-8 gap-y-4 4  gap-x-24">
-                <input type="reset" value="Annulla Modifiche" class="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-400 cursor-pointer"></input>
-                <input type="submit" value="Conferma Modifiche" class="bg-cyan-600 text-white py-2 px-4 rounded-md hover:bg-cyan-500 cursor-pointer "></input>
+                <input name="annulla" type="reset" value="Annulla Modifiche" class="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-400 cursor-pointer"></input>
+                <input name="conferma" type="submit" value="Conferma Modifiche" class="bg-cyan-600 text-white py-2 px-4 rounded-md hover:bg-cyan-500 cursor-pointer "></input>
             </div>
         </form>
     </div>
