d356e7b211453b2aa5f28f1edf969d1c7be0856f
andreaperugini <andreaperugini74@gmail.com>
2024-05-21 16:22:23 +0200

    il div intorno alla tabella non so perche è cosi alto, da rivedere

--- laraProj0/resources/views/analisiDati.blade.php
+++ laraProj0/resources/views/analisiDati.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.basic')
 
 @section('content')
-
-
-
 <div class='m-4 px-10'>
     <div class='flex justify-center'>
         <h1 class='text-5xl font-bold mx-5 mb-8'>Analisi dei dati</h1>
     </div>
 
-    <div class="flex">
-
-        <div class='bg-white text-lg w-1/2 px-5 '>
+    <div class="flex space-x-10">
+        <div class='bg-white text-lg w-1/2 px-5 border rounded border-gray-300'>
             <div class='flex justify-between px-5 gap-x-4 py-3'>
                 <h2 class='w-1/3 font-semibold'>Media pazienti per clinico</h2>
                 <h2>10</h2>
@@ -29,7 +25,6 @@
                     <option>Prova2</option>
                 </select>
             </div>
-
             <div class='flex justify-between px-5 gap-x-4 py-3'>
                 <h2 class='w-1/3 font-semibold'>Numero di eventi totali registrati</h2>
                 <h2>10</h2>
@@ -37,35 +32,26 @@
             <hr>
         </div>
 
-
-
-        <div class="w-1/2 pl-10 items-center ">
-            <input class="w-full" type="text" id="cognomeClinico" placeholder="Cerca per cognome" class=" bg-cyan-50 my-6 appearance-none w-full py-2 px-3
-                 border-0 border-b-2 border-gray-300 focus:border-black
-                  text-gray-700 leading-tight  focus:outline-none">
-            <div class="bg-white">
-
-                <!-- <table class = "table-fixed w-max">
-                    <tr class="w-max flex justify-between"> 
-                        <th scope="col">Paziente</th>
-                        <th scope="col">Numero cambi terapia</th>
-                        
-                    </tr>
-                </table>  da rivedere-->
-
-
+        <div class="w-1/2 border border-gray-300 rounded flex flex-col">
+            <input class="w-full p-2 border-b border-gray-300 rounded-t" type="text" id="cognomeClinico" placeholder="Cerca per cognome">
+            <div class="bg-white overflow-auto flex-grow">
+                <table class="min-w-full bg-white">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2 font-semibold uppercase tracking-wider">Paziente</th>
+                            <th class="px-4 py-2 font-semibold uppercase tracking-wider">Numero cambi terapia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Righe di dati dei pazienti andranno qui -->
+                    </tbody>
+                </table>
             </div>
-
-
         </div>
-
-
-
-
-
+    </div>
+</div>
 @endsection
 
 @section('title')
 Analisi Dati
 @endsection
-
