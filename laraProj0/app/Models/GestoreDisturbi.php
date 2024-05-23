3f00b15ca24bf12ed9c387a680aaf10a3304e466
andreaperugini <andreaperugini74@gmail.com>
2024-05-23 23:07:52 +0200

    gestore disturbi

---
+++ laraProj0/app/Models/GestoreDisturbi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Resources\DistMotorio;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class GestoreDisturbi extends Model
+{
+    use HasFactory;
+
+    public function getDisturbi():Collection {
+        $disturbi = DistMotorio::all();
+        return $disturbi;
+    }
+}
+
