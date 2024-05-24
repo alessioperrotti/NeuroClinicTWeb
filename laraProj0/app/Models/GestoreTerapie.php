4fdb72680b8b3e3cb19b4a3e0fc2d67756cb211f
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-24 15:35:42 +0200

    Models e Seedings

---
+++ laraProj0/app/Models/GestoreTerapie.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Resources\Attivita;
+use App\Models\Resources\Farmaco;
+
+class GestoreTerapie extends Model
+{
+    use HasFactory;
+}
