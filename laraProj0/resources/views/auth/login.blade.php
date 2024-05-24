ac2a1d956e4fcc3903888e6571d697f6fe996136
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-24 11:59:42 +0200

    cursor pointer al login

--- laraProj0/resources/views/auth/login.blade.php
+++ laraProj0/resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                 <br>
                 <input name="password" id="password" type="password" class="border rounded-md h-[60px] w-[336px] p-4 text-xl">
             </div>
-            <input type="submit" class="bg-cyan-600 rounded-xl mt-3 w-[200px] h-[60px] text-white font-semibold hover:bg-cyan-500" value="Login">   
+            <input type="submit" class="bg-cyan-600 rounded-xl mt-3 w-[200px] h-[60px] text-white font-semibold hover:bg-cyan-500 cursor-pointer" value="Login">   
         </form>
     </div>
     <img src="{{ url('images/star3d.png')}}" class="h-[700px] z-0" alt="Sfondo Login">
