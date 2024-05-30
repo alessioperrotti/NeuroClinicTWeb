4d44c350224d72ea57e1d2849948ffe9fd581fdc
Alessio Perrotti <perrottialessio@gmail.com>
2024-05-30 19:33:47 +0200

    Update GestoreClinici.php

--- laraProj0/app/Models/GestoreClinici.php
+++ laraProj0/app/Models/GestoreClinici.php
@@ -6,11 +6,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use App\Models\Resources\Clinico;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class GestoreClinici extends Model
 {
     public function getClinici() : Collection {
         $clinici = Clinico::all();
         return $clinici;
+    }
+
+    public function updateClinico($validatedData, $userClin): bool {
+        DB::beginTransaction();
+        try {
+            $clinico = Clinico::findOrFail($userClin);
+            $clinico->fill($validatedData);
+            $clinico->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Errore durante l\'aggiornamento del clinico: ' . $e->getMessage());
+            return false;
+        }
+        return true;
     }
 }
