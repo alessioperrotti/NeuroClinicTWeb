a599fef305bd2d8b30b4d0efcc64d3727f1e5a43
monks-bb <fra.monaci02@gmail.com>
2024-06-03 18:42:24 +0200

    a

--- laraProj0/resources/views/homeAdmin.blade.php
+++ laraProj0/resources/views/homeAdmin.blade.php
@@ -20,7 +20,12 @@
             </a>
             <div>
                 <nav class="space-x-4 text-white text-sm">
-                    <a href="">LOGOUT</a>
+                    @auth
+                        <a href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="hover:bg-cyan-500 p-1 rounded-lg cursor-pointer ">LOGOUT</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                        @csrf
+                        </form>
+                    @endauth
                 </nav>
             </div>
         </div>
