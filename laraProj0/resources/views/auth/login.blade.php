bdd92287e2c631781dddfe3ecc9c91de61349dfc
Alessio Perrotti <perrottialessio@gmail.com>
2024-06-07 12:25:51 +0200

    validazione login

--- laraProj0/resources/views/auth/login.blade.php
+++ laraProj0/resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="flex place-content-center justify-center">
-    <div class="bg-white rounded-xl h-[480px] w-[400px] absolute z-10 p-8 mt-[100px] items-center">
+    <div class="bg-white rounded-xl min-h-[480px] w-[400px] absolute z-10 p-8 mt-[100px] items-center">
         <h1 class="font-bold font-sans text-3xl text-center mb-12">Accedi</h1>
         <form class="flex flex-col items-center" method="POST" action="{{ route('login')}}"> 
             @csrf 
@@ -12,6 +12,15 @@
                 <label for="user" class="text-xl font-semibold">Username</label>
                 <br>
                 <input name="username" id="user" type="text" class="border rounded-md h-[60px] w-[336px] p-4 text-xl">
+                @if ($errors->first('username'))
+                    <div class="alert alert-danger text-red-500">
+                        <ul>
+                            @foreach ($errors->get('username') as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
             <div class="mb-8"> 
                 <label for="password" class="text-xl font-semibold">Password</label>
@@ -22,6 +31,15 @@
                         <img id="eyeId1" src="{{asset('images/eye-solid.png')}}" alt="toggle" class="w-5">
                     </button>
                 </div>
+                @if ($errors->first('password'))
+                    <div class="alert alert-danger text-red-500">
+                        <ul>
+                            @foreach ($errors->get('password') as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
             <input type="submit" class="bg-cyan-600 rounded-xl mt-3 w-[200px] h-[60px] text-white font-semibold hover:bg-cyan-500 cursor-pointer" value="Login">   
         </form>
