d8bc0f4b8dcdd1494ec5fed9b2c77038b603adff
monks-bb <fra.monaci02@gmail.com>
2024-06-11 19:40:26 +0200

    .

--- laraProj0/resources/views/layouts/basic.blade.php
+++ laraProj0/resources/views/layouts/basic.blade.php
@@ -17,22 +17,20 @@
 <body class="bg-cyan-50">
     <script>
 
-            console.log(document.referrer);
-            function togglePassword(inputId, eyeId) {
-                const input = document.getElementById(inputId);
-                const eye = document.getElementById(eyeId);
-                
-                if (input.type === 'password') {
-                    input.type = 'text';
-                    eye.src ="{{asset('images/eye-slash-solid.png')}}";
-                    eye.alt = 'Hide password';
-                } else {
-                    input.type = 'password';
-                    eye.src = "{{asset('images/eye-solid.png')}}";
-                    eye.alt = 'Show password';
-                }
-        
+        function togglePassword(inputId, eyeId) {
+            const input = document.getElementById(inputId);
+            const eye = document.getElementById(eyeId);
+              
+            if (input.type === 'password') {
+                input.type = 'text';
+                eye.src ="{{asset('images/eye-slash-solid.png')}}";
+                eye.alt = 'Hide password';
+            } else {
+                input.type = 'password';
+                eye.src = "{{asset('images/eye-solid.png')}}";
+                eye.alt = 'Show password';
             }
+        }
    
     </script>
     <header>
