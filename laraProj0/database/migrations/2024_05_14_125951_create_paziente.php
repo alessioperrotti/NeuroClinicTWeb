77cd1b4fffe40bb9aafc836825f94a15fad93977
Alessio Perrotti <perrottialessio@gmail.com>
2024-06-13 14:33:17 +0200

    Revert "Reapply "sistemo regex""
    This reverts commit 04cbba5b3d7eb312bc291f761e9305f9b68a4060.

--- laraProj0/database/migrations/2024_05_14_125951_create_paziente.php
+++ laraProj0/database/migrations/2024_05_14_125951_create_paziente.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->string('civico', 5);
             $table->string('citta', 30);
             $table->string('prov', 2);
-            $table->string('telefono', 15);
-            $table->string('email', 50);
+            $table->string('telefono', 13);
+            $table->string('email', 40);
             $table->string('clinico', 20)->nullable();
             $table->foreign('clinico')->references('username')->on('clinico')->onDelete('set null');
             $table->boolean('terCambiata'); // true se la terapia è stata cambiata e non ancora visualizzata
